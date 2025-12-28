55f972a030d16c66ab37b077d64f7cfedf3b639d
coslor <chris.coslor@gmail.com>
2025-12-27 19:05:06 -0700

    I STILL CANT FUCKING GET SPRITESET TO WORK FUCK FUCK

--- src/invaders.c
+++ src/invaders.c
@@ -6,45 +6,77 @@
 //
 
 
-byte * const Screen = (byte *)0x400; //(byte *)0xc800;
-byte * const Color = (byte *)0xd800;
-byte * Sprites = (byte *)0x340; //0xd800;
+//byte * const Screen = (byte *)0x400; //(byte *)0xc800;
+//byte * const Color = (byte *)0xd800;
+//byte * Sprites = (byte *)0x340; //0xd800;
 
 #define Screen ((char *)0x400)
 
 // make space until 0x2000 for code and data
 
-#pragma region( lower, 0x0a00, 0x2000, , , {code, data} )
+#pragma region( lower, 0xa00, 0x2000, , , {code, data} )
 
 // then space for our sprite data
 
 #pragma section( spriteset, 0)
 
-#pragma region( spriteset, 0x2000, 0x2800, , , {spriteset} )
+#pragma region( spriteset, 0x2000, 0x2040, , , {spriteset} )
 
 // everything beyond will be code, data, bss and heap to the end
 
-#pragma region( main, 0x2800, 0xa000, , , {code, data, bss, heap, stack} )
+#pragma region( main, 0x2040, 0xa000, , , {code, data, bss, heap, stack} )
 
 
 // spriteset at fixed location
 
 #pragma data(spriteset)
 
-const char SpriteData[] = {
-	#embed spd_sprites "invaders.spd"
+/********************************
+ * OK, so make really FUCKING sure that the size of the spriteset array
+ *  is the actual length of the file.
+ *
+*********************************/
+// extern volatile char spriteset[128] = {
+// 	#embed "invaders-both.bin"
+//     //#embed "invaders-181.bin"
+//     // #embed "digitsprites.bin"
+// };
+
+static const char spriteset[128] = 
+// {
+// 	// #embed spd_sprites "invaders.spd"
+//     // #embed "invaders-181.bin"
+
+//     //  sprite_image_0
+//     0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xc3,0xff,0xff,0xff,
+//     0xff,0xf3,0xcc,0x7e,0x33,0xcc,0x7e,0x3f,0xfc,0x7e,0x3f,0xff,0xff,0xff,0xff,0xff,
+//     0xff,0x3f,0xff,0xfc,0x3f,0xff,0xfc,0x0f,0xff,0xf0,0x0f,0xff,0xf0,0x0c,0x00,0x30,
+//     0x0c,0x00,0x30,0x30,0x00,0x0c,0x30,0x00,0x0c,0x00,0x00,0x00,0x00,0x00,0x00,0x01,
+//     //  sprite_image_1
+//     0x0c,0x00,0x30,0x0c,0x00,0x30,0x03,0x00,0xc0,0x03,0xff,0xc0,0x03,0xff,0xc0,0x0f,
+//     0xff,0xf0,0x0c,0x7e,0x30,0x0c,0x7e,0x3c,0x3c,0x7e,0x3c,0x3f,0xff,0xfc,0xff,0xff,
+//     0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xcf,0xff,0xf3,0xcf,0xff,0xf3,0xcc,0x00,0x33,
+//     0xcc,0x00,0x33,0xc3,0x00,0xc3,0xc3,0x00,0xc3,0x00,0x00,0x00,0x00,0x00,0x00,0x01
+// };
+
+{
+    0x0c,0x00,0x30,0x0c,0x00,0x30,0xc3,0x00,0xc3,0xc3,0xff,0xc3,0xc3,0xff,0xc3,0xcf,
+    0xff,0xf3,0xcc,0x7e,0x33,0xcc,0x7e,0x3f,0xfc,0x7e,0x3f,0xff,0xff,0xff,0xff,0xff,
+    0xff,0x3f,0xff,0xfc,0x3f,0xff,0xfc,0x0f,0xff,0xf0,0x0f,0xff,0xf0,0x0c,0x00,0x30,
+    0x0c,0x00,0x30,0x30,0x00,0x0c,0x30,0x00,0x0c,0x00,0x00,0x00,0x00,0x00,0x00,0x01,
+    0x0c,0x00,0x30,0x0c,0x00,0x30,0x03,0x00,0xc0,0x03,0xff,0xc0,0x03,0xff,0xc0,0x0f,
+    0xff,0xf0,0x0c,0x7e,0x30,0x0c,0x7e,0x3c,0x3c,0x7e,0x3c,0x3f,0xff,0xfc,0xff,0xff,
+    0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xcf,0xff,0xf3,0xcf,0xff,0xf3,0xcc,0x00,0x33,
+    0xcc,0x00,0x33,0xc3,0x00,0xc3,0xc3,0x00,0xc3,0x00,0x00,0x00,0x00,0x00,0x00,0x01
 };
 
-//#embed "../build/src/invaders-181.c"
-// static const char invaders_181[128] = {
-// 	#embed "invaders-181.spd"
-// };
+//extern volatile byte value=0;
 
 #pragma data(data)
 
 
 
-//OK, now what happens when we use more sprites than we have?
+
 const int NUM_INVADERS=12;
 
 //Well, as long as we don't try to use a sprite beyond #12 in the spr_
@@ -56,7 +88,7 @@ const int NUM_INVADERS=12;
 
 Invader invaders[NUM_INVADERS] = {
 //active,x,y,speed_x,speed_y,num_images,image_handles,image_num (to start),sprite_num,fps
-    {true, 50,50, 1,0,2,{128,129},0,2,4.0},
+    {true, 50,50, 1,0,2,{128,129},0, 2,4.0},
     {true,100,50, 1,0,2,{128,129},1, 3,4.0},
     {true,150,50, 1,0,2,{128,129},0, 4,4.0},
     {true,200,50, 1,0,2,{128,129},1, 5,4.0},
@@ -84,6 +116,9 @@ int main() {
 
         iocharmap(IOCHM_PETSCII_2);
 
+        // memset((char *)0x2000,0xff,128);
+
+
     // Change colors
 	vic.color_border = VCOL_GREEN;
 	vic.color_back = VCOL_BLACK;
@@ -94,10 +129,11 @@ int main() {
 	vic.spr_mcolor1 = VCOL_WHITE;
 
    	memset(Screen, 32, 1000);
-	memset(Color, 1, 1000);
+	//memset(Color, 1, 1000);
 
 	//memcpy(Font, charset, 2048);
 	//memcpy(Sprites, invaders_181, 128);
+    // memset((char *)0x2000,0xff,128);
 
     //spr_init(Screen);
 
@@ -120,10 +156,22 @@ int main() {
     for (int i=0;i<NUM_INVADERS;i++) {
         Invader *inv=&invaders[i];
         if (inv->alive) {
+            byte color = (i & 15);
+            if (color == 0) {
+                color = 1;
+            }
+            // value =     
+            //         //(((unsigned)&spriteset[0]) / 64);
+            //         (byte)(inv->image_handles[inv->image_num]);
+            //         //(byte)0x80;
+
             vspr_set(inv->sprite_num,
                 inv->x, inv->y,
                 inv->image_handles[inv->image_num],
-                1);
+                //(unsigned)(0xc000 / 64),
+                //value,
+                // 128,
+                color);
         }
     }
     // for (int i=0;i<8; i++) {
@@ -147,15 +195,16 @@ int main() {
             Invader *inv=&invaders[i];
 
             if (inv->alive) {
-                int frames_to_switch=(int)(60.0 / inv->fps);
+                //int frames_to_switch=(int)(60.0 / inv->fps);
 
                 move_invader(inv);
                 if (inv->x <20) {
                     inv->speed_x = abs(inv->speed_x);
                 }
-                else if (inv->x >= 320){
-                    inv->speed_x = -abs(inv->speed_x);
-
+                else {
+                    if (inv->x >= 320){
+                        inv->speed_x = -abs(inv->speed_x);
+                    }
                 }
                 
                 // flip_images(inv);
@@ -166,10 +215,10 @@ int main() {
                 //     inv->image_handles[inv->image_num]);
             }
             else {
- //               vspr_hide(inv->sprite_num);
+                vspr_hide(inv->sprite_num);
         // spr_show(inv->sprite_num, inv->alive);        
             }
-        vic.color_border++; 
+        //vic.color_border++; 
 
         }   //for
 
@@ -185,7 +234,7 @@ int main() {
 		// sort raster IRQs
 		rirq_sort();
 
-        vic.color_border++; 
+        //vic.color_border++; 
         //vic.color_border=9;
     }  
     printf("wtf??\n");
